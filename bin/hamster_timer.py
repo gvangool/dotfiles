69cc42ce88a706c43d1fc5b9f80e682eba95f5e8
Gert Van Gool <gertvangool@gmail.com>
2010-06-10 15:45:26 +0200

    Added a simple monitor to check my hamster-applet

---
+++ bin/hamster_timer.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+import datetime
+import hamster.client
+import logging
+from optparse import OptionParser
+import sys
+import time
+
+try:
+    import pynotify
+except ImportError:
+    logging.error('No pynotify installed')
+    sys.exit(1)
+
+def total_time_of(date=None):
+    s = hamster.client.Storage()
+    if date is None or date == datetime.date.today():
+        facts = s.get_todays_facts()
+    else:
+        facts = s.get_facts(date)
+    total_time = None
+    for fact in facts:
+        end_time = datetime.datetime.now() if fact['end_time'] is None else fact['end_time']
+        t = end_time - fact['start_time']
+        if total_time is None:
+            total_time = t
+        else:
+            total_time += t
+
+    return total_time
+
+def total_time_for_this_week():
+    today = datetime.date.today()
+    dates = [today - datetime.timedelta(days=i) for i in range(0, today.weekday() + 1)]
+    total_time = None
+    for d in dates:
+        t = total_time_of(d)
+        if total_time is None:
+            total_time = t
+        else:
+            total_time += t
+    return total_time
+
+def main():
+    if not pynotify.init('Work monitor'):
+        return
+    parser = OptionParser()
+    parser.add_option('-d', '--delay', type='int', action='store', dest='sleep_time', help='How long must we sleep before checking again?', default=60)
+    options, args = parser.parse_args()
+
+    n = None
+    while True:
+        if n is not None:
+            n.close()
+        title = 'How much did I work?'
+        total_day_time = total_time_of()
+        total_week_time = total_time_for_this_week()
+        msg = 'Worked 8 hours today? %s\n\n' % ('Yes' if total_day_time >= datetime.timedelta(hours=8) else 'No')
+        msg += 'Worked 40 hours this week? %s' % ('Yes' if total_week_time >= datetime.timedelta(hours=40) else 'No')
+        n = pynotify.Notification(title, msg)
+        if total_day_time >= datetime.timedelta(hours=7, minutes=30) or total_week_time >= datetime.timedelta(hours=39, minutes=30):
+            n = pynotify.Notification(title, msg, 'dialog-warning')
+        if total_day_time >= datetime.timedelta(hours=8, minutes=30) or total_week_time >= datetime.timedelta(hours=40, minutes=30):
+            n = pynotify.Notification(title, msg, 'dialog-error')
+        n.set_urgency(pynotify.URGENCY_CRITICAL)
+        n.set_timeout(4)
+        n.show()
+        time.sleep(options.sleep_time)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(levelname)s %(message)s',
+                        filename='/tmp/show_song.log',
+                        filemode='a')
+    total_time = total_time_of()
+    print total_time
+    print 'Worked 8 hours? %s' % ('yes' if total_time >= datetime.timedelta(hours=8) else 'no')
+    total_time = total_time_for_this_week()
+    print total_time
+    print 'Worked 40 hours? %s' % ('yes' if total_time >= datetime.timedelta(hours=40) else 'no')
+    main()
